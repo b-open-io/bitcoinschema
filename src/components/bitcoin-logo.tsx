106b06716d5a845fbbfa9a4f305aaa4c9e6eb201
Luke Rohenaz <rohenaz@gmail.com>
2025-07-16 18:15:03 -0400

    Fix Bitcoin logo centering by adjusting SVG viewBox
    - Changed viewBox from '0 0 200 280' to '20 20 140 240'
    - Crops closer to the actual Bitcoin symbol for better centering
    - Text should now align properly with the visual center of the logo

--- src/components/bitcoin-logo.tsx
+++ src/components/bitcoin-logo.tsx
@@ -5,7 +5,7 @@ interface BitcoinLogoProps {
 export function BitcoinLogo({ className = "h-8 w-8" }: BitcoinLogoProps) {
   return (
     <svg
-      viewBox="0 0 200 280"
+      viewBox="20 20 140 240"
       className={className}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
